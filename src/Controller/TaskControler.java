ffd6d1542ba52c3b56052a6e5ce1f3419c3d5fed
pablorod <jorodriguezce@est.utn.ac.cr>
2023-08-10 14:27:05 -0600

    la clase taskController
     se agrega la clase TaskController

---
+++ src/Controller/TaskControler.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import Views.View;
+
+/**
+ *
+ * @author Pablo Rod
+ */
+public class TaskControler implements Controller<Task> {
+
+      private TaskList task;
+      private View view;
+    
+    
+    @Override
+    public void insert(Task obj) {
+        if (obj.isComplete()) {
+            task.insert(obj);
+            this.readAll();
+        } else {
+            view.displayErrorMessaje("El miembro no está completo.");
+        }
+    }
+
+    @Override
+    public void update(Task obj) {
+       if (obj.isComplete()) {
+            task.update(obj);
+            this.readAll();
+
+        } else {
+            view.displayErrorMessaje("El miembro no se ha eliminado.");
+        }
+    }
+
+    @Override
+    public void delete(Task obj) {
+        if (task.delete(obj)) {
+            this.readAll();
+        } else {
+            view.displayErrorMessaje("El miembro no está completo.");
+        }
+    }
+
+    @Override
+    public void read(Task id) {
+        Task task = taskList.search(id);
+        if (task != null) {
+            this.view.display(task);
+        } else {
+            view.displayErrorMessaje("El miembro no se ha encontrado");
+        }
+    }
+
+    @Override
+    public void readAll() {
+        
+    }
+    
+}
